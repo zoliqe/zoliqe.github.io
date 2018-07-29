e7a3342138a3a308efbc04697db7646ca8954c47
Mike Karas <zoliqe@gmail.com>
2018-07-29 23:50:36 +0200

    ad9850-aa

--- ad9850-aa/main.js
+++ ad9850-aa/main.js
@@ -4,12 +4,20 @@
 const mbitUartService = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'.toLowerCase() //to send TO the microbit
 const mbitUartRxCharacteristic = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'.toLowerCase() //to send TO the microbit
 const mbitUartTxCharacteristic = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'.toLowerCase() //to receive data FROM the microbit
-const connectButton = document.getElementById("connectButton")
+const connectButton = document.querySelector('#connectButton')
 //const freqButton = document.getElementById("freqButton")
-const log = document.getElementById("log")
-const freqInput = document.getElementById('freq')
-//const values = []
+const log = document.querySelector('#log')
+const freqInput = document.querySelector('#freq')
+const reverse = document.querySelector('#reverse')
+const count = 100
+const values = new Array(count)
+values.fill(0)
 
+const canvas = document.querySelector('#graph')
+const ctx = canvas.getContext('2d')
+canvas.height = 500
+canvas.width = count
+/*
 const chart = new Chart('chart', {
   type: 'line',
   data: {
@@ -48,7 +56,7 @@ const chart = new Chart('chart', {
     }
   }
 })
-
+*/
 // chart.canvas.parentNode.style.height = '300px'
 
 function show(text) {
@@ -57,12 +65,30 @@ function show(text) {
 
 function value(val) {
   show(`${freqInput.value}: ${val}`)
-  let data = chart.data.datasets[0].data
-  data.length >= 10 && data.shift()
-  data.push(parseInt(val))
-  //chart.data.datasets[0].data = values
-  chart.update()
-  // console.log(values)
+//  let values = chart.data.datasets[0].data
+  values.shift()
+  values.push((getBaseLog(1.5, parseInt(val) + 1)) + 1)
+  // chart.update()
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.globalAlpha = 1.0
+  let v
+  for (let i = 0; i < values.length; ++i) {
+    v = values[i]
+    let color = '150,0,0'
+    if (v < 1.5) {
+      color = '0,150,0'
+    } else if (v < 3) {
+      color = '0,150,150'
+    }
+    ctx.fillStyle = 'rgb(' + color + ')'
+    ctx.fillRect(i, canvas.height, 1, -v*40)
+  }
+  reverse.innerHTML = v > 10 ? '>10' : v.toFixed(1)
+}
+
+function getBaseLog(x, y) {
+  return Math.log(y) / Math.log(x);
 }
 
 const bluetoothSearchOptions = {
@@ -112,7 +138,7 @@ function connectClicked(e) {
     [rx, tx] = rxandtx
     mbit = new MicroBitUART(rx, tx)
     mbit.valueChanged = val => value(val)
-    show('Ready!')
+    show('Connected')
     connectButton.disabled = true
     setInterval(() => mbit.send('?'), 200)
   }).catch(error => console.log(error))
